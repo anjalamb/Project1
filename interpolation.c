6fb1d084bf5129e7a7dae532c2df6bd0c965b302
anjalamb <aanjalamb@gmail.com>
2017-04-02 02:18:13 +0530

    hi

--- interpolation.c
+++ interpolation.c
@@ -7,8 +7,8 @@ int main()
    char line[50];
    int i,j,n, N_old, N_new,y; double x_old[20] ;
    double x_new[20],f_new[20], f_old[20],sum,pf;	/*pf correspoding to lambda*/
-   FILE *inptr, *fptr;
-   
+  
+  hello  
    inptr= fopen("sample_data.dat", "r");
    fptr= fopen("interpolate.dat","w");
    fgets(line,sizeof(line), inptr);
@@ -18,11 +18,7 @@ int main()
       sscanf(line,"%lf %lf", &x_old[n],&f_old[n]);
       n++;
    }
-   N_old= n;
-   for(int m=1; m<n; m++)
-   { printf("E= %lf \t cross section %lf \n ", x_old[m], f_old[m] ); }
-   for ( y=0; y <=200 ; y +=2)
-   {
+   
    
    for( i=1; i<n ;i++ )
    {
